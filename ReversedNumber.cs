d03e5c3ad6c8db5a01b356f7c793e9bac53a503d
Pipistrella <o.vardarova@gmail.com>
2015-09-29 12:57:07 +0300

    Part of Methods Homework

---
+++ ReversedNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class ReversedNumber
+    {
+        static string ReverseNumber(string number)
+        {
+            char[] charArray = number.ToCharArray();
+            Array.Reverse( charArray );
+            return new string(charArray);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter the number to reverse");
+            string number = Console.ReadLine();
+            Console.WriteLine(ReverseNumber(number));
+        }
+    }
+
